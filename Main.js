2d840c2f9fb9595b9731b721cf8e2cf4aea1e037
EchoBlr <echo.blur@yandex.ru>
2022-11-07 17:45:46 +0300

    Saving project for another device

--- Main.js
+++ Main.js
@@ -1,2 +1,34 @@
-//there will be somethig
-let Code = 'full';
+let buttonWho
+let buttonWhat
+
+class lightBox {
+    constructor(whom,what){
+        this._whom = whom;
+        this._what = what;
+        }
+    getText(){
+        switch (whom){
+            case "Мама":
+                break
+            case "Жена":
+                break
+            case "Сестра":
+                break
+            case "Папа":
+                break
+        }
+    }
+    static getWhom(){
+        const possibleWhom = ['Мама','Жена','Сестра', "Папа"]
+
+    }
+
+
+    static getWhat(){
+        const possibleWhat = ["День рожденье","Новый год"]
+    }
+}
+
+buttonWho = document.getElementById('first')
+buttonWhat = document.getElementById('second')
+buttonWho.addEventListener('click',()=>{buttonWho.style.backgroundColor = 'red'})
